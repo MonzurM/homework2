ee46f009f28ff990e7bba38010b7331fb70b55b7
MonzurM <monzur1337@gmail.com>
2019-03-01 04:38:35 -0500

    quiz7

---
+++ IdeaProjects/quiz7/src/IntegerToString.java
@@ -0,0 +1,10 @@
+public class IntegerToString {
+
+    public static void main(String[] args) {
+    int i = 2019;
+
+    String s = String.valueOf(i);
+
+    System.out.println(s);     //Output : 2015
+}
+}
---
+++ IdeaProjects/quiz7/src/StringToInteger.java
@@ -0,0 +1,10 @@
+public class StringToInteger {
+    public static void main(String[] args)
+    {
+        String s = "2019";
+
+        int i = Integer.valueOf(s);
+
+        System.out.println(i);
+    }
+}
